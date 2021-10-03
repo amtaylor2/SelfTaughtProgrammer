d1615c233793a8e6998fff14479bb45d902b832f
Andrew Taylor <andrewmtaylor2@gmail.com>
2021-10-03 17:50:58 -0500

    add challenge questions

---
+++ 17ch/challenges.py
@@ -0,0 +1,20 @@
+#Challenge 1
+
+#Write a regular expression that matches
+#the word Dutch in The Zen of Python.
+
+#Challenge 2
+
+#Come up with a regular expression that
+#matches all the digits in the string
+#Arizona 479, 501, 870. California 209,
+# 213, 650.
+
+#Challenge 3
+
+#Create a regular expression that matches
+#any word that starts with any character
+#and is followed by two o's. Then use 
+#Python's re module to match boo and loo
+#in the sentence
+#The ghost that says boo haunts the loo.
