92375b78935cae0015d0c7ea032d4b47be46eacb
Andrew Taylor <andrewmtaylor2@gmail.com>
2021-10-02 19:00:29 -0500

    add completed challenges

---
+++ 16ch/challenges.sh
@@ -0,0 +1,27 @@
+#Challenge 1
+
+#Print Self-taught in Bash
+
+echo Self-taught
+
+#Challenge 2
+
+#Navigate to your home directory from
+#another directory using an absolute
+#and relative path.
+
+cd /home ##absolute
+
+cd .. relative
+
+#Challenge 3
+
+#Create an env var called $python_projects
+#that is an absolute path to the directory
+#where you keep your Python files. Save 
+#the var in your .profile file and then use
+#the command cd $python_projects to 
+#navigate there.
+
+export python_projects="C/Users/amtay/Desktop/SelfTaughtProgrammer/"
+
